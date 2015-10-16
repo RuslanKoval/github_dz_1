d5d40a9cb21bfdcf37f0d5c8fc30d80e88454356
RuslanKoval <ruslan.koval21@gmail.com>
2015-10-16 13:41:47 +0400

    fibonachi

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <p>Числа фибоначи</p>
+    <p>
+        <?php
+            function fibonacci($n)
+            {
+                if ($n < 3) {
+                    return 1;
+                }
+                else {
+                    return fibonacci($n-1) + fibonacci($n-2);
+                }
+            }
+            for ($n = 1; $n <= 16; $n++) {
+                echo(fibonacci($n) . ", ");
+            }
+            echo("...\n")
+        ?>
+    </p>
+</body>
+</html>
