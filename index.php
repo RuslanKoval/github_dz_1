a705b28acc7d35e60eb9b1123851e8313fe638db
RuslanKoval <ruslan.koval21@gmail.com>
2015-10-16 13:48:04 +0400

    factorial

--- index.php
+++ index.php
@@ -23,5 +23,22 @@
             echo("...\n")
         ?>
     </p>
+    <p>Факториал</p>
+    <p>
+        <?php
+        function factorial($n)
+        {
+            if ($n == 0) {
+                return 1;
+            } else {
+                return $n * factorial($n - 1);
+            }
+        }
+        for ($n = 0; $n <= 16; $n++) {
+            echo($n . "! = " . factorial($n) . "\n");
+        }
+        echo("...\n")
+        ?>
+    </p>
 </body>
 </html>
